cabf0503e2af6680988b26ca67638f002980f29b
chaunhat2507 <chaunhat2507@gmail.com>
2024-04-19 09:07:03 +0700

    .

--- src/main/java/org/asm/Product.java
+++ src/main/java/org/asm/Product.java
@@ -1,6 +1,16 @@
 package org.asm;
 
+import java.util.HashSet;
+
 public record Product(String bcode, String title, int quantity, double price) {
+    private static final HashSet<String> existed = new HashSet<>();
+
+    public Product {
+        if (!existed.add(bcode)) {
+            throw new IllegalArgumentException("Invalid bcode");
+        }
+    }
+
     @Override
     public String toString() {
         return bcode + " | " + title + " | " + quantity + " | " + price;
